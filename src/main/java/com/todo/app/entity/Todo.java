5766e552d10f1b2b4e6875ed11e7c6fd8a0dd29c
kamechan <kamechan@example.com>
2024-01-12 16:47:47 +0900

    Entity作成

---
+++ src/main/java/com/todo/app/entity/Todo.java
@@ -0,0 +1,21 @@
+package com.todo.app.entity;
+
+import java.sql.Date;
+import lombok.Data;
+
+@Data
+public class Todo {
+    
+    private long id;
+    private String title;
+    private int done_flg;
+    private String time_limit;
+
+    public Long getID() {
+        return id;
+    }
+
+    public void setID(Long id) {
+        this.id = id;
+    }
+}
